87ff57a758ab522f48e53d532329d848a38475c9
ed <ed.frost@hotmail.com>
2023-11-01 08:03:38 -0400

    updated stats

--- Get-ChumpballStats.ps1
+++ Get-ChumpballStats.ps1
@@ -40,3 +40,7 @@ If (Test-Path -Path "$rootFolder\config\bets.csv") {
 }
 
 Write-StatsOutput -RosterInput $rosterStats -toHTML -AddTable $bets
+
+git status
+git add .
+git commit -m 'updated stats'
